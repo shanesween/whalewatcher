d6bbdce62f9b50872cbf7d4d22c1de126416f456
Shane Sweeney <shanep.sween@gmail.com>
2021-03-23 16:03:14 -0700

    Change theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,7 +5,7 @@ import red from '@material-ui/core/colors/red';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#647f98',
+      main: '#617793',
     },
     secondary: {
       main: '#19857b',
