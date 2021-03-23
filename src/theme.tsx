13d84a79a8b8b3cac8caae2a2c69f0588e46eab5
Shane Sweeney <shanep.sween@gmail.com>
2021-03-22 21:58:48 -0700

    Add material ui

---
+++ src/theme.tsx
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#647f98',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
