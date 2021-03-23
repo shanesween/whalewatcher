c962819896a1b50a4e2535432f0ee492466b1849
Shane Sweeney <shanep.sween@gmail.com>
2021-03-22 21:59:21 -0700

    add card, css baseline, gray whale img

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Container, Typography } from '@material-ui/core';
+import theme from './theme';
+import OutlinedCard from './Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm">
+        <Box component="div" style={{ height: '100vh' }}>
+          <Box>
+            <Typography variant="h3" gutterBottom>
+              Newport Whales
+            </Typography>
+          </Box>
+          <Box>
+            <OutlinedCard />
+          </Box>
+        </Box>
+      </Container>
     </div>
   );
 }
