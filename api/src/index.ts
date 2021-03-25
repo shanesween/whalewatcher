b47cbb85d9e0e6355a2cde51f18f88fe98c585eb
Shane Sweeney <shanep.sween@gmail.com>
2021-03-25 09:46:43 -0700

    Table split by row in array

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,12 +4,17 @@ const url = 'https://newportwhales.com/whalecount.html'
 
 const AxiosInstance = axios.create()
 
+
 AxiosInstance.get(url)
   .then(
     response => {
       const html = response.data;
-      const $ = cheerio.load(html)
-      const table = $('h1:contains("Recent Counts") ~ table').text()
-      console.log('table', table);
+      //normalize whitespace gave me most probles, simple fix! read. the. docs.
+      const $ = cheerio.load(html, { xmlMode: true, normalizeWhitespace: true, decodeEntities: true })
+      const tableArray = $('h1:contains("Recent Counts") ~ table').html().split('</tr>')
+
+      console.log(tableArray);
+      console.log(typeof tableArray);
+
     }
   ).catch(console.error)
