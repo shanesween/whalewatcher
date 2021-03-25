3e5df3dad2d313cb9bba0e2d796f2a04e72d5e1d
Shane Sweeney <shanep.sween@gmail.com>
2021-03-25 12:01:00 -0700

    add types for response data

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,40 @@ const url = 'https://newportwhales.com/whalecount.html'
 const AxiosInstance = axios.create()
 
 
+enum Species {
+  GrayWhale = "GRAY WHALE",
+  BlueWhale = "BLUE WHALE",
+  FinWhale = "FIN WHALE",
+  HumpbackWhale = "HUMPBACK WHALE",
+  MinkeWhale = "MINKE WHALE",
+  Orca = "ORCA",
+  BrydeWhale = "BRYDE'S WHALE",
+
+  CommonDolphin = "COMMON DOLPHIN",
+  PacificDolphin = "PACIFIC WHITE-SIDED DOLPHIN",
+  BottlenoseDolphin = "BOTTLENOSE DOLPHIN",
+  RissoDolphin = "RISSO'S DOLPHIN",
+
+  MakoShark = "MAKO SHARK",
+  ThresherShark = "THRESHER SHARK",
+  HammerheadShark = "HAMMERHEAD SHARK",
+  GreatWhiteShark = "GREAT WHITE SHARk",
+  FalseKillerWhale = "FALSE KILLER WHALE",
+  Sunfish = "SUNFISH"
+}
+
+
+interface Mammal {
+  name: Species,
+  count: number
+}
+
+interface MammalData {
+  date: string
+  totalCount: number
+  mammals: Mammal[]
+}
+
 AxiosInstance.get(url)
   .then(
     response => {
