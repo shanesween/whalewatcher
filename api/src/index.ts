c313be4412ccd9e570f47affaf6e25b028e65206
Shane Sweeney <shanep.sween@gmail.com>
2021-03-25 18:45:04 -0700

    parse string, build data object

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,56 +1,51 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+import { Mammal, DailyData } from '../types/index';
+
 const url = 'https://newportwhales.com/whalecount.html'
 
 const AxiosInstance = axios.create()
-
-
-enum Species {
-  GrayWhale = "GRAY WHALE",
-  BlueWhale = "BLUE WHALE",
-  FinWhale = "FIN WHALE",
-  HumpbackWhale = "HUMPBACK WHALE",
-  MinkeWhale = "MINKE WHALE",
-  Orca = "ORCA",
-  BrydeWhale = "BRYDE'S WHALE",
-
-  CommonDolphin = "COMMON DOLPHIN",
-  PacificDolphin = "PACIFIC WHITE-SIDED DOLPHIN",
-  BottlenoseDolphin = "BOTTLENOSE DOLPHIN",
-  RissoDolphin = "RISSO'S DOLPHIN",
-
-  MakoShark = "MAKO SHARK",
-  ThresherShark = "THRESHER SHARK",
-  HammerheadShark = "HAMMERHEAD SHARK",
-  GreatWhiteShark = "GREAT WHITE SHARk",
-  FalseKillerWhale = "FALSE KILLER WHALE",
-  Sunfish = "SUNFISH"
-}
-
-
-interface Mammal {
-  name: Species,
-  count: number
-}
-
-interface MammalData {
-  date: string
-  totalCount: number
-  mammals: Mammal[]
-}
 
 AxiosInstance.get(url)
   .then(
     response => {
       const html = response.data;
+
       //normalize whitespace gave me most probles, simple fix! read. the. docs.
       const $ = cheerio.load(html, { xmlMode: true, normalizeWhitespace: true, decodeEntities: true })
       const tableArray = $('h1:contains("Recent Counts") ~ table').html().split('</tr>')
+      let responseDataArray: DailyData[] = []
 
-      tableArray.forEach((elem, i) => {
-        const rowArray: string[] = elem.split('</td> <td>')
-        console.log(rowArray);
+      tableArray.forEach((row, i) => {
+        //daily <tr>
+        const rowArray: string[] = row.split('</td> <td>')
+        //first <td> - date
+        const date: string = rowArray[0].split('<td>')[1]
+        //second <td> - string of mammals and count
+        const mammalArray: string[] = rowArray[1] && rowArray[1].split('</td>')[0].split(', ')
+
+        let dailyTotalCount = 0
+        let dailyData: DailyData = { date, dailyTotalCount, mammals: [] }
+
+        mammalArray && mammalArray.forEach((mammal, i) => {
+          //daily count for each species, add to dailyTotal
+          const countSpecies = parseFloat(mammal.match(/[a-z]+|[^a-z]+/gi)[0].replace(/,/g, ''))
+          dailyTotalCount += countSpecies
+
+          //species name
+          const species = mammal.replace(/^[, ]+|[, ]+$|[, ]+/g, " ").match(/\D/g).join('').trim()
+
+          //mammalObject
+          const mammalObject: Mammal = { name: species, count: countSpecies }
+
+          dailyData.mammals = [...dailyData.mammals, mammalObject]
+        })
+
+        dailyTotalCount = isNaN(dailyTotalCount) ? 0 : dailyTotalCount
+        dailyData.dailyTotalCount = dailyTotalCount
+        dailyData.date && responseDataArray.push(dailyData)
+        console.log(responseDataArray);
+
       })
-
     }
   ).catch(console.error)
---
+++ api/types/index.ts
@@ -0,0 +1,35 @@
+export enum Species {
+  GrayWhale = "GRAY WHALE",
+  BlueWhale = "BLUE WHALE",
+  FinWhale = "FIN WHALE",
+  HumpbackWhale = "HUMPBACK WHALE",
+  MinkeWhale = "MINKE WHALE",
+  Orca = "ORCA",
+  BrydeWhale = "BRYDE'S WHALE",
+
+  CommonDolphin = "COMMON DOLPHIN",
+  PacificDolphin = "PACIFIC WHITE-SIDED DOLPHIN",
+  BottlenoseDolphin = "BOTTLENOSE DOLPHIN",
+  RissoDolphin = "RISSO'S DOLPHIN",
+
+  MakoShark = "MAKO SHARK",
+  ThresherShark = "THRESHER SHARK",
+  HammerheadShark = "HAMMERHEAD SHARK",
+  GreatWhiteShark = "GREAT WHITE SHARk",
+  FalseKillerWhale = "FALSE KILLER WHALE",
+  Sunfish = "SUNFISH",
+
+  BadWeather = "BADWEATHER"
+}
+
+
+export interface Mammal {
+  name: string,
+  count: number
+}
+
+export interface DailyData {
+  date: string,
+  dailyTotalCount: number
+  mammals: Mammal[]
+}
