bfbba75e294611036f7e9f034d7f9eb9a394976f
Shane Sweeney <shanep.sween@gmail.com>
2021-03-28 22:43:06 -0700

    dynamo table

---
+++ api/db/index.ts
@@ -0,0 +1,31 @@
+// import entire SDK
+import AWS from 'aws-sdk';
+
+
+AWS.config.update({ region: "us-east-1" });
+
+const dynamodb = new AWS.DynamoDB()
+
+var params = {
+  TableName: "Sightings",
+  KeySchema: [
+    { AttributeName: "date", KeyType: "HASH" },  //Partition key
+    { AttributeName: "count", KeyType: "RANGE" }  //Sort key
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "date", AttributeType: "S" },
+    { AttributeName: "count", AttributeType: "N" }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+};
+
+dynamodb.createTable(params, function (err, data) {
+  if (err) {
+    console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+  } else {
+    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+  }
+});
