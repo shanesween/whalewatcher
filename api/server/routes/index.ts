1a46e59ad6b3938df0fb1e7becdfe98cfe613853
Shane <shanep.sween@gmail.com>
2021-04-14 22:39:04 -0700

    cors access

--- api/server/routes/index.ts
+++ api/server/routes/index.ts
@@ -2,6 +2,14 @@ import { Router } from "express";
 
 const router = Router()
 
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 router.use("/sightings", require("./sightings"))
 
 router.use((req, res, next) => {
