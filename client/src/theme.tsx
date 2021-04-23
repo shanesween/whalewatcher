ad9555cb70e6e939b0ebb55fff641fc6f7307030
Shane <shanep.sween@gmail.com>
2021-04-23 15:11:56 -0700

    Theme fix for last commit

--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -2,7 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
 // A custom theme for this app
-const theme = createMuiTheme({
+let theme = createMuiTheme({
   palette: {
     primary: {
       main: '#778AA2',
@@ -23,5 +23,17 @@ const theme = createMuiTheme({
     }
   },
 });
+theme = {
+  ...theme,
+  overrides: {
+    MuiTab: {
+      root: {
+        [theme.breakpoints.up("xs")]: {
+          minWidth: '33%'
+        }
+      }
+    }
+  }
+}
 
 export default theme;
